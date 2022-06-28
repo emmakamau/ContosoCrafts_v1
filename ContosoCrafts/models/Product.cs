87d75527f39c38ffd8f6bce93e2e21e1f9ecf280
Emmaculate Kamau <ekamau@637capital.com>
2022-06-28 11:57:40 +0300

    Add project files.

---
+++ ContosoCrafts/models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    //Create our product model
+    public class Product
+    {
+        public string? Id { get; set; }
+        public string? Maker { get; set; }
+        //Match Json property name to product name Image
+        [JsonPropertyName("img")]
+        public string? Image { get; set; }
+        public string? Url { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int[]? Ratings { get; set; }
+
+        //Serialize object instantiated
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
---
+++ ContosoCrafts/services/JsonFileProductService.cs
@@ -0,0 +1,32 @@
+﻿using ContosoCrafts.WebSite.Models;
+using System.Text.Json;
+
+namespace ContosoCrafts.services
+{
+    public class JsonFileProductService
+    {
+        //Constructor
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            //webHostEnvironment is a service offering a service to our JsonFileProductService
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        //Creates a path to the JsonFile on the webroot
+        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "product.json");
+
+        //Retrives product items from our Json file - Deserializing to make our product instances
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+            Product[]? products = JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                            new JsonSerializerOptions
+                            {
+                                PropertyNameCaseInsensitive = true,
+                            });
+            return products;
+        }
+    }
+}
