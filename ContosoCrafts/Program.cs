8f90b288d75dd57bf5fc147e7729284292d376e8
Emmaculate Kamau <ekamau@637capital.com>
2022-06-28 14:54:49 +0300

    Serialize models and return json data

--- ContosoCrafts/Program.cs
+++ ContosoCrafts/Program.cs
@@ -1,4 +1,6 @@
 using ContosoCrafts.services;
+using ContosoCrafts.WebSite.Models;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +27,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapGet("/products", context =>
+{
+    var products = app.Services.GetService<JsonFileProductService>().GetProducts();
+    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+    return context.Response.WriteAsync(json);
+});
 
 app.Run();
